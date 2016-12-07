a16865659f1a19b62858c91a304872bf864b6e01
Thomas Hopkins <thopki1@ext.uber.com>
2016-12-07 10:19:34 -0800

    UCHAT-275 Add config option to hide locale setting

--- components/user_settings/user_settings_display.jsx
+++ components/user_settings/user_settings_display.jsx
@@ -491,6 +491,9 @@ export default class UserSettingsDisplay extends React.Component {
                 </div>
             );
         }
+        if (global.window.mm_config.EnableLocaleSetting !== 'true') {
+            languagesSection = null;
+        }
 
         let themeSection;
         if (global.mm_config.EnableThemeSelection !== 'false') {

c40b7fa4b280bd0144152fd14bd5994b5dbecec5
David Meza <dmeza@ext.uber.com>
2017-09-12 22:08:47 -0500

    UCHAT-66 inline playback for gif attachments

--- components/file_attachment.jsx
+++ components/file_attachment.jsx
@@ -257,6 +257,11 @@ export default class FileAttachment extends React.Component {
 
         let fileAttachment;
         if ((type === 'image' || type === 'svg') && !this.props.compactDisplay && this.props.displayType !== 'preview') {
+            let thumbnailUrl = getFileThumbnailUrl(fileInfo.id);
+            if (Utils.isGIFImage(fileInfo.extension)) {
+                thumbnailUrl = getFileUrl(fileInfo.id);
+            }
+
             fileAttachment = (
                 <div className='post-image__preview'>
                     <a
@@ -265,7 +270,7 @@ export default class FileAttachment extends React.Component {
                     >
                         <img
                             className='post-image__image'
-                            src={getFileThumbnailUrl(fileInfo.id)}
+                            src={thumbnailUrl}
                         />
                     </a>
                     <div className='post-image__info'>

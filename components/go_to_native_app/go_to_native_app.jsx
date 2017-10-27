95a6c4cf2ba6abcf07690e537dba0a0237c63758
David Meza <dmeza@ext.uber.com>
2017-10-26 23:37:22 -0500

    Add landing page when deep linking to native app
    - Add protocol detection for desktop app
    - fixup to protocol
    - Moved logic to new router
    - Renamed folder to be more descriptive. Created proper PureComponent. Fix all issues related to string replacements. Fixed timeOut not working.
    - Updating styles for deep linking page
    - Fix order of regexp to replace protocol

---
+++ components/go_to_native_app/go_to_native_app.jsx
@@ -0,0 +1,104 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import React, {PureComponent} from 'react';
+import {FormattedMessage} from 'react-intl';
+import safeOpenProtocol from 'custom-protocol-detection';
+
+import MattermostLogoSvg from 'images/logo.svg';
+
+export default class GoNativeApp extends PureComponent {
+    constructor(props) {
+        super(props);
+        this.state = {
+            protocolUnsupported: false,
+            browserUnsupported: false,
+        };
+    }
+
+    render() {
+        const {protocolUnsupported, browserUnsupported} = this.state;
+
+        let nativeLocation = window.location.href.replace('/vault#', '');
+        nativeLocation = nativeLocation.replace(/^(https|http)/, 'mattermost');
+
+        safeOpenProtocol(nativeLocation,
+            () => this.setState({protocolUnsupported: true}),
+            () => setTimeout(redirectWeb, 3000),
+            () => this.setState({browserUnsupported: true})
+        );
+
+        let goNativeAppMessage = null;
+        if (protocolUnsupported) {
+            goNativeAppMessage = (
+                <FormattedMessage
+                    id='get_app.protocolUnsupported'
+                    defaultMessage='Unable to open Mattermost.'
+                />
+            );
+        } else if (browserUnsupported) {
+            goNativeAppMessage = (
+                <FormattedMessage
+                    id='get_app.browserUnsupported'
+                    defaultMessage='This browser does not support opening applications.'
+                />
+            );
+        } else {
+            goNativeAppMessage = (
+                <FormattedMessage
+                    id='get_app.systemDialogMessage'
+                    defaultMessage='Please click `Open Mattermost` if you see the system dialog.'
+                />
+            );
+        }
+
+        // prompt user to download in case they don't have the mobile app.
+        return (
+            <div className='get-app get-app--android'>
+                <img
+                    src={MattermostLogoSvg}
+                    className='get-app__logo'
+                />
+                <div className='get-app__launching'>
+                    <FormattedMessage
+                        id='get_app.launching'
+                        defaultMessage='Launching...'
+                    />
+                </div>
+                <div className='get-app__status'>
+                    {goNativeAppMessage}
+                </div>
+                <div>
+                    <div className='get-app__alternative'>
+                        <FormattedMessage
+                            id='get_app.ifNothingPrompts'
+                            defaultMessage='If nothing prompts from browser,'
+                        />
+                    </div>
+                    <a
+                        href='/downloads'
+                        className='btn btn-primary get-app__download'
+                    >
+                        <FormattedMessage
+                            id='get_app.downloadMattermost'
+                            defaultMessage='Download & run Mattermost'
+                        />
+                    </a>
+                </div>
+                <a
+                    href='/'
+                    className='btn btn-secondary get-app__continue'
+                >
+                    <FormattedMessage
+                        id='get_app.continueToBrowser'
+                        defaultMessage='Continue to web site'
+                    />
+                </a>
+            </div>
+        );
+    }
+}
+
+function redirectWeb() {
+    window.location = window.location.href.replace('/vault#', '');
+}

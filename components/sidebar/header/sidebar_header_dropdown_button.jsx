3a8587dd153f5d7515152355d26fd8cb561af881
Sourab Kumar Keshri <sourabkumar.keshri@uber.com>
2019-09-30 17:23:22 +0530

    Bug Fix for misalignment of username on increasing the length of the sidebar

--- components/sidebar/header/sidebar_header_dropdown_button.jsx
+++ components/sidebar/header/sidebar_header_dropdown_button.jsx
@@ -95,7 +95,7 @@ export default class SidebarHeaderDropdownButton extends React.PureComponent {
                         id='headerInfo'
                         className='header__info'
                     >
-                        {teamNameWithToolTip}
+                       {teamNameWithToolTip}<br/>
                         <div
                             id='headerUsername'
                             className='user__name'
